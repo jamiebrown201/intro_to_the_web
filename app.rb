5c0806ee367c335f29e412332e02920263da0489
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-12-07 16:21:34 +0000

    add views folder with index.erb and seperated out html

--- app.rb
+++ app.rb
@@ -1,20 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World"
-end
-
-get '/secret' do
-  "<h1>secretfhdsjkfhsdjkfhds</h1>"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src=http://bit.ly/1eze8aE >
-  </div>"
-end
-
-
-get '/dog' do
-  "dog"
+  erb(:index)
 end
