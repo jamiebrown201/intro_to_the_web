259245bea914eaab59cb3a07a35bdc1616c2c9f8
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-12-07 18:10:10 +0000

    add named cat post and cat form

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 
 
+
 get '/random-cat' do
   @cat_name = ["Jimmy", "Oscar", "Mog"].sample
   erb(:index)
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @cat_name = params[:name]
   erb(:index)
